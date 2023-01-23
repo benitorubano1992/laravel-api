39d9ca6d9e3f0b5e95b4964686de9d5a14a33ba2
benitorubano1992 <rubanobenito@gmail.com>
2023-01-23 16:40:01 +0100

    add new routes to api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,12 +8,36 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $projects = Project::with(['type', 'tecnologies'])->paginate(3);
-        return response()->json([
-            'success' => true,
-            'results' => $projects
-        ]);
+        if ($request->has('type_id')) {
+            $projects = Project::with(['type', 'tecnologies'])->where('type_id', $request->type_id)->paginate(3);
+            return response()->json([
+                "success" => true,
+                "results" => $projects
+            ]);
+        } else {
+            $projects = Project::with(['type', 'tecnologies'])->paginate(3);
+            return response()->json([
+                'success' => true,
+                'results' => $projects
+
+            ]);
+        };
+    }
+    public function show($slug)
+    {
+        $project = Project::with(['type', 'tecnologies'])->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                "success" => true,
+                "results" => $project
+            ]);
+        } else {
+            return response()->json([
+                "success" => false,
+                "error" => "There isn't a project with that slug"
+            ]);
+        }
     }
 }
